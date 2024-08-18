08a0652a8dd5970025124b56e3f84cb26863776a
lokesh k <lokesh@softwaregurukulam.com>
2024-08-18 19:13:00 +0000

    Updated code_app_service.tf

--- Terraform_Scripts/code_app_service/code_app_service.tf
+++ Terraform_Scripts/code_app_service/code_app_service.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
 
 # Define the Resource Group
 resource "azurerm_resource_group" "example" {
-  name     = "rg-bookportal-resources"
+  name     = "rg-reqportal-resources"
   location = "East US"  # Replace with your desired location
 }
 
 # Define the Standard App Service Plan
 resource "azurerm_app_service_plan" "standard_plan" {
-  name                = "rg-Requestportal-app-service-plan"
+  name                = "rg-reqportal-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   kind                = "App"
@@ -23,7 +23,7 @@ resource "azurerm_app_service_plan" "standard_plan" {
 
 # Define the Code-based App Service
 resource "azurerm_app_service" "code_service" {
-  name                = "rg-bookportal-code-app-service"
+  name                = "rg-reqportal-code-app-service"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.standard_plan.id
